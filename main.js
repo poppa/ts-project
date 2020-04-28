62d6d33fe3ce5f9bcce3310c0c031e4064b3633a
Pontus Östlund <ponost@roxen.com>
2020-04-28 21:30:19 +0200

    Initial commit
    I seems to be working fairly well

---
+++ main.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+const { args, copy, run } = require(`./lib`)
+
+const start = async () => {
+  const { argv } = args()
+  const results = []
+
+  if (!argv['skip-init']) {
+    results.push(await run.yarnInit())
+  }
+
+  if (!argv['skip-add']) {
+    results.push(await run.yarnAdd())
+  }
+
+  if (!argv['skip-copy-config']) {
+    results.push(await copy.config())
+  }
+
+  if (!argv['skip-copy-src']) {
+    results.push(await copy.src())
+  }
+
+  if (!argv['skip-copy-jest']) {
+    results.push(await copy.jest())
+  }
+
+  if (!argv['skip-copy-tests']) {
+    results.push(await copy.tests())
+  }
+
+  if (!argv['skip-build']) {
+    results.push(await run.build())
+  }
+
+  if (!argv['skip-test']) {
+    results.push(await run.jest())
+  }
+
+  if (results.every((r) => r)) {
+    console.log(`\n`)
+    console.log(`Everything seems to fly. Happy coding...\n`)
+  } else {
+    console.log(`\n`)
+    console.log(`Something wasn't correct. Check output above...\n`)
+  }
+
+  console.log(`results:`, results)
+}
+
+start().catch((e) => console.error(`Main error:`, e))
